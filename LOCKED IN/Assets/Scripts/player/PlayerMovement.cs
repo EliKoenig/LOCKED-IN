29cb77b63ed5ef64b2cab2b02d9f03f1a2c14108
Eli Koenig <elifireguyfburg@gmail.com>
2024-08-23 12:54:29 -0400

    Feat: Crouching

--- LOCKED IN/Assets/Scripts/player/PlayerMovement.cs
+++ LOCKED IN/Assets/Scripts/player/PlayerMovement.cs
@@ -14,10 +14,24 @@ public class PlayerMovement : MonoBehaviour
     public float groundCheckRadius = 0.4f;
     public LayerMask groundMask;
 
+    public float fallMultiplier = 2.5f; // Multiplier for falling gravity
 
+
+    // Crouch variables
+    private Vector3 playerScale = new Vector3(1f, 1f, 1f);
+    private Vector3 crouchScale = new Vector3(1f, 0.6f, 1f);
+    public float crouchSpeed = 6f;
+    public float scaleSpeed = 5f;
 
     Vector3 velocity;
     bool isGrounded;
+    bool isCrouching => playerScale.y - crouchScale.y > .1f;
+
+    void Start()
+    {
+        // Store the original height of the character controller
+      
+    }
 
     // Update is called once per frame
     void Update()
@@ -34,15 +48,43 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed *Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftControl)) // Use your preferred crouch key
+        {
+            // Smoothly lerp to crouch scale
+            transform.localScale = Vector3.Lerp(transform.localScale, crouchScale, Time.deltaTime * scaleSpeed);
+            speed = crouchSpeed; // Reduce speed while crouching
+
+
+        }
+        else
+        {
+            // Smoothly lerp to stand scale
+            transform.localScale = Vector3.Lerp(transform.localScale, playerScale, Time.deltaTime * scaleSpeed * 1.5f);
+            speed = 12f; // Reset speed to original
+
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        velocity.y += gravity * Time.deltaTime;
+        // Apply gravity
+        if (velocity.y < 0)
+
+        {
+            velocity.y += gravity * fallMultiplier * Time.deltaTime; // Apply increased gravity when falling
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime; // Normal gravity when rising
+        }
 
         controller.Move(velocity * Time.deltaTime);
+
+
+
     }
 }
