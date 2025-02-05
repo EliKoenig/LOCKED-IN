dd7d94890843ee93aa8c1be5d018e2a5d607ae40
Eli Koenig <elifireguyfburg@gmail.com>
2025-02-05 15:07:40 -0500

    com

---
+++ LOCKED IN/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayControlGame()
+    {
+        SceneManager.LoadScene("Control Scene");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
